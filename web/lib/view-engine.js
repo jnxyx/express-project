f0729279199a105a37180900ef65951c722d8c64
jnxyx <821969066@qq.com>
2016-09-29 11:37:43 +0800

    moban change

--- web/lib/view-engine.js
+++ web/lib/view-engine.js
@@ -3,12 +3,17 @@
  * 
  */
 var path = require('path');
+var ejs = require('ejs');
 
 module.exports = function(app) {
 
-	var dir = __dirname.replace('lib', '');
+    var dir = __dirname.replace('lib', '');
 
-    app.set('views', path.join( dir , 'views') );
-    app.set('view engine', 'jade');
+    app.set('views', path.join(dir, 'views'));
+    app.engine('html', ejs.__express);
+
+    // app.set('view engine', 'ejs');
+
+    // app.set('view engine', 'jade');
 
 }

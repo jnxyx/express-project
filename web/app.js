7dfd44969d25de37f11104edbc42651d349042d4
jnxyx <821969066@qq.com>
2016-09-29 10:52:34 +0800

    change  the  dir of app-init

--- web/app.js
+++ web/app.js
@@ -8,16 +8,10 @@ var bodyParser = require('body-parser');
 var app = express();
 var initApp = require('./lib/init'); 
 
+/**
+ * 初始化
+ */
 initApp(app);
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- web/lib/init.js
+++ web/lib/init.js
@@ -6,9 +6,32 @@
 var initViewEngine = require('./view-engine.js'); 
 var initRoutes = require('./routes-initialize.js'); 
 
-module.exports = function(app){
+/**
+ * [配置]
+ * 
+ */
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
+module.exports = function(app){
+	/**
+	 * 路由视图设置
+	 */
 	initViewEngine(app);
 	initRoutes(app);
-	
+
+	/**
+	 * 其他配置
+	 */
+	//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+	app.use(logger('dev'));
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({ extended: false }));
+	app.use(cookieParser());
+	app.use(express.static(path.join(__dirname, 'public')));
+
 }

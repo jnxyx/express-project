841bd0209a635895cf57a59747d2a7b09a1ee8db
jnxyx <821969066@qq.com>
2016-09-29 10:56:16 +0800

    change dir

--- web/app.js
+++ web/app.js
@@ -1,9 +1,4 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
 
 var app = express();
 var initApp = require('./lib/init'); 

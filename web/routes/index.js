d1691d46386271e4fd433eaa847bbc77b36054fd
jnxyx <821969066@qq.com>
2016-09-29 16:08:55 +0800

    change header

--- web/routes/index.js
+++ web/routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index.html', { title: 'Express' });
+    res.render('index.html', { title: 'Express' }, function(err, str) {
+        if (err) {
+            return req.next(err);
+        }
+        res._headerNames["Server"] = "Server";
+        res._headers["Server"] = "SB Server.NT";
+        res._headers["x-powered-by"] = "JN-xyx";
+        res.send(str);
+    });
 });
 
 module.exports = router;

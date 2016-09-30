e2cdb145db19e7b634a75aacbae894efb3d2e8fc
jnxyx <821969066@qq.com>
2016-09-30 11:28:56 +0800

    change the dir

--- web/routes/index.js
+++ web/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index.html', { title: 'Express' }, function(err, str) {
+    res.render('home/index.html', { title: 'Express' }, function(err, str) {
         if (err) {
             return req.next(err);
         }

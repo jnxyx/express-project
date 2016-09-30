147a3288d380a6f9566301bcbadba92b7b33b92e
jnxyx <821969066@qq.com>
2016-09-30 10:08:24 +0800

    clear the dir and code

--- web/lib/init.js
+++ web/lib/init.js
@@ -37,4 +37,38 @@ module.exports = function(app){
 	app.use(cookieParser());
 	app.use(express.static(path.join(dir, 'public')));
 
+	/**
+	 * 异常处理
+	 */
+	handleError.call(app);
+
+}
+
+function handleError(){
+	var app = this;
+
+	app.use(function(req, res, next) {
+	  var err = new Error('Not Found');
+	  err.status = 404;
+	  next(err);
+	});
+
+	if (app.get('env') === 'development') {
+	  app.use(function(err, req, res, next) {
+	    res.status(err.status || 500);
+	    res.render('error', {
+	      message: err.message,
+	      error: err
+	    });
+	  });
+	}
+
+	app.use(function(err, req, res, next) {
+	  res.status(err.status || 500);
+	  res.render('error', {
+	    message: err.message,
+	    error: {}
+	  });
+	});
+
 }
